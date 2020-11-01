f77ebfddfa741b755a624b2f20efe5520f7f65db
eglove <hello@ethang.email>
2020-11-01 09:24:04 -0600

    link colors for pages

--- src/templates/Blog.js
+++ src/templates/Blog.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import SingleBlog from '../components/Blog/SingleBlog';
 import Seo from '../components/SEO';
+import { PageWrapper } from '../styles/PageStyles';
 
 // Currently the /blog page shows full blogs, if that is changed to summary or something else,
 // use this file to show th full blog on it's own URL
 export default function singleBlogPage({ data: { post } }) {
   return (
-    <>
+    <PageWrapper>
       <Seo title={post.title} image={post.mainImage?.asset?.fluid?.src} />
       <SingleBlog post={post} />
-    </>
+    </PageWrapper>
   );
 }
 
--- src/templates/Project.js
+++ src/templates/Project.js
@@ -3,16 +3,17 @@ import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import SingleProject from '../components/Portfolio/SingleProject';
 import Seo from '../components/SEO';
+import { PageWrapper } from '../styles/PageStyles';
 
 // Currently the /portfolio page shows all information on projects
 // if that is changed to summary or something else, use this file to show the full
 // project on it's own URL
 export default function Project({ data: { project } }) {
   return (
-    <>
+    <PageWrapper>
       <Seo title={project.name} image={project.image?.asset?.fluid?.src} />
       <SingleProject project={project} />
-    </>
+    </PageWrapper>
   );
 }
 
