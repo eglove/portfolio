ac68a84e696a9220482de30c33240c5a6ec8ba36
Ethan Glover <hello@ethang.email>
2021-02-08 19:48:06 -0600

    Update date!: 	modified:   src/pages/courses.js

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -19,7 +19,7 @@ export default function CoursePage() {
         description="Personally recommended development courses."
       />
       <h1>Recommended Courses</h1>
-      <p>Last Update: Feb. 5, 2021</p>
+      <p>Last Update: Feb. 8, 2021</p>
       <CourseList courses={courses} otherCourses={otherCourses} />
     </PageWrapper>
   );
